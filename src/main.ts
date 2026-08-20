90d56a2a76d3b4cddd55803352e06187eb492f2c
thepixelwriter <gauravsharma8813@gmail.com>
2026-08-20 20:03:18 +0530

    Add on-device debug console for iPhone testing without a Mac

--- src/main.ts
+++ src/main.ts
@@ -2,5 +2,10 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
 
+// Opt-in on-device console (open with ?debug=1) - lets us see errors on iPhone Safari without a Mac.
+if (new URLSearchParams(window.location.search).get('debug') === '1') {
+  import('eruda').then((eruda) => eruda.default.init());
+}
+
 platformBrowserDynamic().bootstrapModule(AppModule)
   .catch(err => console.log(err));
